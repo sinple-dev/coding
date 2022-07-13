368b63a193f98e60be10d057f3fb9589ef70d7b0
sinple <wlstjq11111@gmail.com>
2022-07-13 23:10:40 +0900

    programmers coding practice

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -1,16 +1,17 @@
 package com.coding;
 
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Main main = new Main();
-        int[] numbers = {1,1,1,1,1};
-
+        int[][] dungeons = {{80,20}, {50,40}, {30,10}};
+        System.out.println(main.solution(80, dungeons));
 
     }
-
-
 
 
 }
