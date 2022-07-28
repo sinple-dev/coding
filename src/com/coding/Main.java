8c21b2be572219eba41bf8329133eeb1c2215ceb
sinple <wlstjq11111@gmail.com>
2022-07-29 00:25:41 +0900

    sosu

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -1,16 +1,62 @@
 package com.coding;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         Main main = new Main();
+        main.solution("011");
+    }
 
+    boolean[] visited;
+    HashSet<Integer> map;
+
+    public int solution(String numbers) {
+
+        visited = new boolean[numbers.length()];
+        map = new HashSet<>();
+
+        backTracking(0, numbers, "");
+
+        return map.size();
+    }
+
+    public void backTracking(int depth, String numbers, String current) {
+        if (depth == numbers.length()) {
+            return;
+        }
+
+        for (int i=0; i< numbers.length(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+
+                String tmp = current + numbers.charAt(i);
+
+                if (chk(Integer.parseInt(tmp))) {
+                    map.add(Integer.parseInt(tmp));
+                }
+
+                backTracking(depth+1, numbers, tmp);
+                visited[i] = false;
+
+            }
+
+        }
+
+    }
+    public boolean chk(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (i != n && n % i == 0)
+                return false;
+        }
+
+        if (n <= 1) return false;
+        return true;
     }
 
 
---
+++ src/com/coding/programmers/full_search_sosu.java
@@ -0,0 +1,62 @@
+package com.coding.programmers;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class full_search_sosu {
+
+    public static void full_search_sosu(String[] args) {
+        full_search_sosu main = new full_search_sosu();
+        main.solution("011");
+    }
+
+    boolean[] visited;
+    HashSet<Integer> map;
+
+    public int solution(String numbers) {
+
+        visited = new boolean[numbers.length()];
+        map = new HashSet<>();
+
+        backTracking(0, numbers, "");
+
+        return map.size();
+    }
+
+
+    public void backTracking(int depth, String numbers, String current) {
+        if (depth == numbers.length()) {
+            return;
+        }
+
+        for (int i=0; i< numbers.length(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+
+                String tmp = current + numbers.charAt(i);
+
+                if (chk(Integer.parseInt(tmp))) {
+                    map.add(Integer.parseInt(tmp));
+                }
+
+                backTracking(depth+1, numbers, tmp);
+                visited[i] = false;
+
+            }
+
+        }
+
+    }
+    public boolean chk(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (i != n && n % i == 0)
+                return false;
+        }
+
+        if (n <= 1) return false;
+        return true;
+    }
+
+}
