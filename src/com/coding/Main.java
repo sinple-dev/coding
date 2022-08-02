e57124af49dc8b7617bb3cee1d592e3855342373
sinple <wlstjq11111@gmail.com>
2022-08-02 19:15:43 +0900

    kakao full shot

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -1,20 +1,14 @@
 package com.coding;
 
 import java.io.IOException;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
 
-        int[][] wires = {{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
-
-        main.solution(9, wires);
-
+        main.solution(2, new String[]{"N~F=0", "R~T>2"});
     }
-
-
 
 
 
---
+++ src/com/coding/programmers/kakao_fullshot.java
@@ -0,0 +1,70 @@
+package com.coding.programmers;
+
+public class kakao_fullshot {
+
+    public static void full_search_sosu(String[] args) {
+        kakao_fullshot main = new kakao_fullshot();
+        main.solution(2, new String[]{"N~F=0", "R~T>2"});
+    }
+
+    String[] player = {"A", "C", "F", "J", "M", "N", "R", "T"};
+    String[] item;
+    int answer = 0;
+
+    public int solution(int n, String[] data) {
+        boolean[] visit = new boolean[8];
+        item = data;
+        dfs("", visit);
+        return answer;
+    }
+
+    public void dfs(String name, boolean[] visit) {
+
+        if (name.length() == 8) {
+            if (check(name)) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int i = 0; i < player.length; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                String sb = name + player[i];
+                dfs(sb, visit);
+                visit[i] = false;
+            }
+        }
+    }
+
+    public boolean check(String sb) {
+        boolean isChk = true;
+        for (int i = 0; i < item.length; i++) {
+            int a = sb.indexOf(item[i].charAt(0));
+            int b = sb.indexOf(item[i].charAt(2));
+
+            char op = item[i].charAt(3);
+
+            int diff = Integer.parseInt(String.valueOf(item[i].charAt(4)));
+
+            if (String.valueOf(op).equals("=")) {
+                if (!(Math.abs(a - b) == diff+1)) {
+                    isChk = false;
+                    break;
+                }
+            } else if (String.valueOf(op).equals(">")) {
+                if (Math.abs(a - b) <= diff+1) {
+                    isChk = false;
+                    break;
+                }
+            } else {
+                if (Math.abs(a - b) >= diff+1) {
+                    isChk = false;
+                    break;
+                }
+            }
+        }
+
+        return isChk;
+    }
+}
