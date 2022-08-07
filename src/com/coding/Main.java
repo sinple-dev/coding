02f8e1d21edfe748be6541df3fdcb3aeb7eb5dea
sinple <wlstjq11111@gmail.com>
2022-08-07 18:45:47 +0900

    practice rectangle

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -1,51 +1,31 @@
 package com.coding;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
 
-        main.solution(new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"});
+        System.out.println(main.solution(2, 3));
 
     }
 
-//    public int[] solution(int[][] data) {
-//
-//        Queue<int[]> queue = new LinkedList<>();
-//
-//        int[] answer = new int[data.length];
-//        int time = 0;
-//
-//
-//        for (int i = 0; i < data.length; i++) {
-//            int[] print = data[i];
-//
-//            while(true) {
-//
-//                if(queue.isEmpty()) {
-//                    queue.add(print);
-//                    time += print[1];
-//                    answer[i] = i;
-//                    break;
-//                } else {
-//
-//                    queue.poll();
-//                }
-//
-//            }
-//
-//        }
-//
-//
-//        return answer;
-//
-//    }
+    public long solution(int w, int h)
+    {
+        long gcd = getGcd(w, h);
+        return ((long) w * h) - (((w / gcd) + (h / gcd) - 1) * gcd);
+    }
 
+    private int getGcd(int n, int m)
+    {
+        while (m != 0)
+        {
+            int r = n % m;
+            n = m;
+            m = r;
+        }
+        return n;
+    }
 }
 
