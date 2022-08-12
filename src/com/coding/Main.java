cede3c431b673a0142db745dce075145e815d436
sinple <wlstjq11111@gmail.com>
2022-08-12 13:32:54 +0900

    scoville

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -1,28 +1,39 @@
 package com.coding;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
 
-        System.out.println(main.solution("I"));
+
+        System.out.println(main.solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
 
     }
 
-    public int solution(String word) {
+    public int solution(int[] scoville, int K) {
         int answer = 0;
-        int[] arr = {781, 156, 31, 6, 1};
-        String dic = "AEIOU";
-        for (int i = 0; i < word.length(); i++) {
-            answer += dic.indexOf(word.charAt(i)) * arr[i] + 1;
+
+        // arraylist 쓰면 효율성이 떨어져
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int no : scoville) {
+            q.add(no);
         }
 
-        return answer;
+        while (q.size() > 1 && q.peek() < K) {
+            int a = q.poll();
+            int b = q.poll();
+            int tmp = a + (b * 2);
+            q.add(tmp);
+            answer++;
+        }
 
+        return (q.size() < 2 && q.peek() < K) ? -1 : answer;
     }
 
 }
+
+
 
