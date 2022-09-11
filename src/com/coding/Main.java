e021f48e8e986766434c06241246000a54002994
sinple <wlstjq11111@gmail.com>
2022-09-11 10:32:06 +0900

    practice 1level

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
+//        System.out.println(
+//                main.solution(new String[]{"mislav", "stanko", "mislav", "ana", "aba"}, new String[]{"stanko", "ana", "mislav"})
+//        );
         System.out.println(
-                main.solution(new String[]{"mislav", "stanko", "mislav", "ana", "aba"}, new String[]{"stanko", "ana", "mislav"})
+                main.solution(10)
         );
     }
 
---
+++ src/com/coding/programmers/practice_evenodd.java
@@ -0,0 +1,24 @@
+package com.coding.programmers;
+
+import java.io.IOException;
+
+public class practice_evenodd {
+
+    public static void main(String[] args) throws IOException {
+        practice_evenodd main = new practice_evenodd();
+//        System.out.println(
+//                main.solution(new String[]{"mislav", "stanko", "mislav", "ana", "aba"}, new String[]{"stanko", "ana", "mislav"})
+//        );
+        System.out.println(
+                main.solution(10)
+        );
+    }
+
+    public String solution(int num) {
+        if (num % 2 == 0) return "Even";
+        else return "Odd";
+    }
+}
+
+
+
