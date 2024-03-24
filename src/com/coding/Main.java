d8c4a59cbfa1a8e1d65cac8f6a0f655cbef07f1d
sinple <43742364+sinple-dev@users.noreply.github.com>
2024-03-24 20:08:17 +0900

    self number

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -4,15 +4,8 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
-        System.out.println(
-                main.solution(new int[]{1, 2}, new int[]{3, 4})
-        );
-
     }
-
 }
 
 
