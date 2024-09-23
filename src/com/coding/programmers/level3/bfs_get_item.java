2f95bb499c5c02d4b1ac8fc77d876f12107f3225
sinple <43742364+sinple-dev@users.noreply.github.com>
2024-09-24 08:41:34 +0900

    study

--- src/com/coding/programmers/level3/bfs_get_item.java
+++ src/com/coding/programmers/level3/bfs_get_item.java
@@ -3,11 +3,6 @@ package com.coding.programmers.level3;
 import java.util.*;
 
 public class bfs_get_item {
-	static int[][] map = new int[101][101];
-	static int[] dI = {1, -1, 0, 0};
-	static int[] dJ = {0, 0,1,-1};
-	static boolean[][] visited = new boolean[101][101];
-	static int result =0;
 
 	public static void main(String[] args) {
 		bfs_get_item main = new bfs_get_item();
@@ -19,35 +14,34 @@ public class bfs_get_item {
 		System.out.println(main.solution(rectangle, characterX, characterY, itemX, itemY));
 	}
 
+	static int[][] map = new int[101][101];
+	static int[] dI = {1, -1, 0, 0};
+	static int[] dJ = {0, 0, 1, -1};
+	static boolean[][] visited = new boolean[101][101];
+	static int result = 0;
+
 	public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
 		drawMap(rectangle);
-		bfs(characterX*2, characterY*2, itemX*2, itemY*2);
+		bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
 		return result;
 	}
 
-	static void drawMap(int[][] rectangle)
-	{
-		for(int[] rect : rectangle)
-		{
-			int startI = rect[1] *2;
-			int endI = rect[3] *2;
+	static void drawMap(int[][] rectangle) {
+		for (int[] rect : rectangle) {
+			int startI = rect[1] * 2;
+			int endI = rect[3] * 2;
 
-			int startJ = rect[0]*2;
-			int endJ = rect[2]*2;
+			int startJ = rect[0] * 2;
+			int endJ = rect[2] * 2;
 
-			for(int i=startI;i<=endI;i++)
-			{
-				for(int j=startJ;j<=endJ;j++)
-				{
-					if(i == startI || i == endI || j == startJ || j == endJ)
-					{
-						if(map[i][j] ==2)
-						{
+			for (int i = startI; i <= endI; i++) {
+				for (int j = startJ; j <= endJ; j++) {
+					if (i == startI || i == endI || j == startJ || j == endJ) {
+						if (map[i][j] == 2) {
 							continue;
 						}
 						map[i][j] = 1;
-					}
-					else{
+					} else {
 						map[i][j] = 2;
 					}
 				}
@@ -57,7 +51,7 @@ public class bfs_get_item {
 
 	static void bfs(int characterX, int characterY, int itemX, int itemY) {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[]{characterY, characterX, 0}); //x , y , step
+		q.add(new int[] {characterY, characterX, 0}); //x , y , step
 
 		while (!q.isEmpty()) {
 			int[] current = q.poll();
@@ -66,7 +60,7 @@ public class bfs_get_item {
 			int currentStep = current[2];
 
 			if (currentI == itemY && currentJ == itemX) {
-				result = currentStep/2;
+				result = currentStep / 2;
 				return;
 			}
 
@@ -74,11 +68,10 @@ public class bfs_get_item {
 				int nextI = currentI + dI[i];
 				int nextJ = currentJ + dJ[i];
 
-				if(nextI>=1 && nextI <=100 && nextJ>=1 && nextJ <=100)
-				{
+				if (nextI >= 1 && nextI <= 100 && nextJ >= 1 && nextJ <= 100) {
 					if (!visited[nextI][nextJ] && map[nextI][nextJ] == 1) {
 						visited[nextI][nextJ] = true;
-						q.add(new int[]{nextI, nextJ, currentStep + 1});
+						q.add(new int[] {nextI, nextJ, currentStep + 1});
 					}
 				}
 			}
