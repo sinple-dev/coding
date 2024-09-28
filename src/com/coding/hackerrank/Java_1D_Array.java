d63fd5ba167295a7364d6eb6fb751193d0b667b3
sinple <43742364+sinple-dev@users.noreply.github.com>
2024-09-28 19:54:41 +0900

    study

---
+++ src/com/coding/hackerrank/Java_1D_Array.java
@@ -0,0 +1,54 @@
+package com.coding.hackerrank;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+class Java_1D_Array {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int q = scan.nextInt();
+		while (q-- > 0) {
+			int n = scan.nextInt();
+			int leap = scan.nextInt();
+
+			int[] game = new int[n];
+			for (int i = 0; i < n; i++) {
+				game[i] = scan.nextInt();
+			}
+
+			System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+		}
+		scan.close();
+	}
+
+	public static boolean canWin(int leap, int[] game) {
+		// Return true if you can win the game; otherwise, return false.
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		queue.add(0);
+
+		while(!queue.isEmpty()) {
+			int i = queue.poll();
+
+			if (i > game.length - 1) return true;
+
+			if (i < 0 || game[i] == 1) continue;
+
+			game[i] = 1;
+
+			// if(i+leap < game.length) queue.add(i + 1);
+			// if (i - 1 > 0) queue.add(i - 1);
+			// if(i+leap < game.length) queue.add(i + leap);
+			queue.add(i + 1);    // 오른쪽으로 한 칸 이동
+			queue.add(i - 1);    // 왼쪽으로 한 칸 이동
+			queue.add(i + leap); // 점프
+		}
+
+		return false;
+	}
+
+}
